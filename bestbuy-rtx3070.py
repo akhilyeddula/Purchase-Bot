9aec31e76aa2474a948deb9d65c0e7d28392e0a1
Akhil Yeddula <akhilyeddula2004@outlook.com>
2021-04-17 19:54:23 -0400

    Added a new Python script for the RTX 3070 and worked on the Canvas test

--- bestbuy-rtx3070.py
+++ bestbuy-rtx3070.py
@@ -4,8 +4,4 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
 
 # This is where the actual execution of the web driver starts.
-driver.get("https://hcpscanvasproject.azurewebsites.net/")
-currentUrl = driver.current_url
-print(currentUrl)  
-
-driver.quit()
+driver.get()
